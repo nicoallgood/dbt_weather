b8d7c5b1df8f639cdf6b77afdfb397f352cd754e
Benoit <benoit@kestra.io>
2024-07-23 18:18:24 +0200

    [fix] staging naming

--- models/mart/mart_agg_day.sql
+++ models/mart/mart_agg_day.sql
@@ -2,5 +2,5 @@ SELECT
   _city AS city,
   DATE(_time) AS date_time,
   AVG(_temperature) AS avg_temperature
-FROM {{ ref("staging_temparature") }}
+FROM {{ ref("staging_temperature") }}
 GROUP BY _city, DATE(_time)
